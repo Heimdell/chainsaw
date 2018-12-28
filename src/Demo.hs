fa12846b0ba113541c1b8f1ac31ccd3ee029c6c5
Kirill Andreev <hindmost.one@gmail.com>
2018-12-28 13:06:11 +0400

    Even nicer

--- src/Demo.hs
+++ src/Demo.hs
@@ -63,22 +63,29 @@ type Proof = Map Address Account
 
 ---- On-chain access ----------------------------------------------------------
 
-instance {-# OVERLAPPING #-} Reading Address Account M where
+instance {-# OVERLAPPING #-}
+    Reading Address Account M
+  where
     tryGet address = do
         Map.lookup address <$> lift (gets statusAccounts)
 
-instance {-# OVERLAPPING #-} Writing Address Account M where
+instance {-# OVERLAPPING #-}
+    Writing Address Account M
+  where
     store address account = do
         lift $ modify $ withStatusAccounts $ Map.insert address account
 
-instance {-# OVERLAPPING #-} Reading Fees TheFees M where
+instance {-# OVERLAPPING #-}
+    Reading Fees TheFees M
+  where
     tryGet Fees = do
         Just <$> lift (gets paymentFees)
 
 -- Actions over accounts
-touchAccount   addr   = change addr $ \acc -> acc { accountNonce   = accountNonce   acc + 1 }
-unTouchAccount addr   = change addr $ \acc -> acc { accountNonce   = accountNonce   acc - 1 }
-changeBalance  addr d = do
+touchAccount   addr = change addr $ \acc -> acc { accountNonce = accountNonce acc + 1 }
+unTouchAccount addr = change addr $ \acc -> acc { accountNonce = accountNonce acc - 1 }
+
+changeBalance addr d = do
     acc <- retrieve addr
     change addr $ \acc -> acc { accountBalance = accountBalance acc + d }
     return $ Map.singleton addr acc
